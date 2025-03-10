8ea35c8f8cdfc9d9b9f282c7f123a47aec06406b
paras-verma7454 <parasverma7454@gmail.com>
2025-03-10 22:11:25 +0530

    fix prompt issues

--- src/data/Prompt.tsx
+++ src/data/Prompt.tsx
@@ -64,6 +64,7 @@ files:{
 - For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse  photos/gifs  Do not download the images, only link to them in image tags.\n\n
 - Don't use localStorage 
 - cursor pointer for buttons
+- make it beautiful and modern
 - dark theme 
 
    `,
