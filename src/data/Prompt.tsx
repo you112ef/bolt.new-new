467f0e8fd25b526fa06a09e5d039d009b2015ba2
paras-verma7454 <parasverma7454@gmail.com>
2025-03-10 22:23:48 +0530

    updated

--- src/data/Prompt.tsx
+++ src/data/Prompt.tsx
@@ -61,11 +61,11 @@ files:{
 
 - By default, this template supports tSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.
 
-- For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse  photos/gifs  Do not download the images, only link to them in image tags.\n\n
+- For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports tSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\n
 - Don't use localStorage 
 - cursor pointer for buttons
-- make it beautiful and modern
-- dark theme 
+- make the ui/ux beautiful 
+-  provide dark theme and light theme option
 
    `,
 
